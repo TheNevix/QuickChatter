dc2a50eceba88aea32eaf47e9a0f49a508613ce9
TheNevix <ducelliersaimen@gmail.com>
2024-12-22 09:25:11 +0100

    Added the connect user control and a on click event.

--- QuickChatter.Client/ViewModels/vmMainWindow.cs
+++ QuickChatter.Client/ViewModels/vmMainWindow.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QuickChatter.Client.ViewModels
 {
-    internal class vmMainWindow
+    public class vmMainWindow
     {
+        public ICommand ConnectCommand { get; }
+
+        public vmMainWindow()
+        {
+            ConnectCommand = new RelayCommand(ConnectToServer, CanButtonClick); ;
+        }
+
+        private void ConnectToServer(object sender)
+        {
+            // Your button click logic here
+            System.Diagnostics.Debug.WriteLine("Button clicked!");
+        }
+
+        private bool CanButtonClick(object parameter)
+        {
+            // Return true or false depending on whether the button should be enabled
+            return true;
+        }
     }
 }
