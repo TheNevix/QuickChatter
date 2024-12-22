24f119a8abc48add359a25c37bfa0fd07c05bb21
TheNevix <ducelliersaimen@gmail.com>
2024-12-22 08:32:02 +0100

    Updated MVVM setup.

--- QuickChatter.Client/Views/MainWindow.xaml.cs
+++ QuickChatter.Client/Views/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace QuickChatter.Client
+namespace QuickChatter.Client.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
